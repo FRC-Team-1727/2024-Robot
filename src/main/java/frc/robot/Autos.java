241d9323ba69b3b7582edb8734ddb7cd2a7d13dc
unknown <ghfeibusch@gmail.com>
2024-01-23 14:57:01 -0500

    initial commit

---
+++ src/main/java/frc/robot/Autos.java
@@ -0,0 +1,7 @@
+package frc.robot;
+
+import com.pathplanner.lib.auto.NamedCommands;
+
+public class Autos {
+    public static void registerNamedCommands() {}
+}
---
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -0,0 +1,17 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkFlex;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import static frc.robot.Constants.IntakeConstants.*;
+
+public class IntakeSubsystem extends SubsystemBase {
+    private final CANSparkFlex intake = new CANSparkFlex(kIntakePort, MotorType.kBrushless);
+
+    public IntakeSubsystem() {}
+
+    public void intake(double spd) {
+        intake.set(spd);
+    }
+}
