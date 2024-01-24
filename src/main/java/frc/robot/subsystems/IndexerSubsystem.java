92d39014ebf80374b40954c171ca671eef567b7d
George Feibusch <ghfeibusch@gmail.com>
2024-01-24 18:49:09 -0500

    subsystem drafting

--- src/main/java/frc/robot/subsystems/IndexerSubsystem.java
+++ src/main/java/frc/robot/subsystems/IndexerSubsystem.java
@@ -4,9 +4,7 @@ import com.revrobotics.CANSparkFlex;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 
 import static frc.robot.Constants.IndexerConstants.*;
 
